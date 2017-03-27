c3651d39c3e214d4aad40bcf214a821bb36909af
daniel.aguado.mt <daniel.aguado@mediatechsolutions.es>
2017-03-27 11:30:58 +0200

    Added network partitions check type

--- check_rabbit_server.py
+++ check_rabbit_server.py
@@ -5,11 +5,14 @@ from base_rabbit_check import BaseRabbitCheck
 
 
 class RabbitCheckServer(BaseRabbitCheck):
+    def __init__(self, *args, **kwargs):
+        super(RabbitCheckServer, self).__init__(*args, **kwargs)
+        self.percentage = 0
     """
     performs a nagios compliant check on a single queue and
     attempts to catch all errors. expected usage is with a critical threshold of 0
     """
-    type = make_option("--type", dest="type", help="Type of check - mem, fd, proc, sockets, disk", type="string", default='%2F')
+    type = make_option("--type", dest="type", help="Type of check - mem, fd, proc, sockets, disk, network_partitions", type="string", default='%2F')
 
     def makeUrl(self):
         """
@@ -31,7 +34,7 @@ class RabbitCheckServer(BaseRabbitCheck):
         if not self.options.type:
             self.rabbit_note = "Missing type"
             return False
-        if not (self.options.type == 'mem' or self.options.type == 'fd' or self.options.type == 'proc' or self.options.type == 'sockets' or self.options.type == 'disk'):
+        if not self.options.type in ('mem', 'fd', 'proc', 'sockets', 'disk', 'network_partitions'):
             self.rabbit_note = "Type of " + self.options.type + " is incorrect"
             return False
         return True
@@ -41,9 +44,13 @@ class RabbitCheckServer(BaseRabbitCheck):
         return result
 
     def parseResult(self, data):
-        for result in data:
-            if string.split(result['name'], '@')[1] in self.options.hostname:
-                nodeData = result
+        nodeData = None
+#        for result in data:
+#            if string.split(result['name'], '@')[1] in self.options.hostname:
+#                nodeData = result
+        if len(data) > 0:
+          nodeData = data[0]
+
         if nodeData:
             if self.options.type == 'mem':
                 if nodeData['mem_alarm'] is True:
@@ -53,6 +60,10 @@ class RabbitCheckServer(BaseRabbitCheck):
                 if nodeData['disk_free_alarm'] is True:
                     return Response(CRITICAL, 'disk alarm triggered!')
                 self.percentage = nodeData[self.options.type + "_free_limit"] / (nodeData[self.options.type + "_free"] / 100.0)
+            if self.options.type == 'network_partitions':
+                if len(nodeData['partitions']) > 0:
+                    return Response(CRITICAL, 'network partition detected!')
+                self.percentage = len(nodeData['partitions'])
 
             if self.options.type == 'fd':
                 self.percentage = nodeData[self.options.type + "_used"] / (nodeData[self.options.type + "_total"] / 100.0)
